7ee472c0d904cf2934e756129a3262b4b27bea3b
JackieWu <wujindou1@163.com>
2023-12-22 14:24:33 +0800

    Update containers.py

--- sequence_aligner/containers.py
+++ sequence_aligner/containers.py
@@ -13,6 +13,13 @@ class TrainingExample:
     labels: IntList
 
 @dataclass
+class SpanTrainingExample:
+    input_ids:IntList
+    attention_mask:IntList
+    start_ids:IntList
+    end_ids:IntList
+
+@dataclass
 class PredictExample:
     input_ids: IntList
     attention_masks: IntList
@@ -51,3 +58,30 @@ class TraingingBatch:
         self.input_ids = torch.LongTensor(input_ids)
         self.attention_masks = torch.LongTensor(masks)
         self.labels = torch.LongTensor(labels)
+
+
+class SpanTraingingBatch:
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __init__(self, examples: List[TrainingExample]):
+        self.input_ids: torch.Tensor
+        self.attention_masks: torch.Tensor
+        self.start_ids: torch.Tensor
+        self.end_ids: torch.Tensor
+
+        input_ids: IntListList = []
+        masks: IntListList = []
+        start_ids: IntListList = []
+        end_ids: IntListList = []
+
+        for ex in examples:
+            input_ids.append(ex.input_ids)
+            masks.append(ex.attention_masks)
+            start_ids.append(ex.start_ids)
+            end_ids.append(ex.end_ids)
+        self.input_ids = torch.LongTensor(input_ids)
+        self.attention_masks = torch.LongTensor(masks)
+        self.start_id = torch.LongTensor(start_ids)
+        self.end_ids = torch.LongTensor(end_ids)
+

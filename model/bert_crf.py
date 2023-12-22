8a5401f65cb8d4d4a250f7f1e6fe03da0557f2d7
JackieWu <wujindou1@163.com>
2023-12-22 14:25:39 +0800

    Update bert_crf.py

--- model/bert_crf.py
+++ model/bert_crf.py
@@ -59,3 +59,41 @@ class BertSoftmaxForNer(BertPreTrainedModel):
             outputs = (loss,) + outputs
         return outputs  # (loss), scores, (hidden_states), (attentions)
 
+
+class BertSoftmaxForSpan(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertSoftmaxForSpan, self).__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.start_fc = nn.Linear(config.hidden_size, 4)
+        self.end_fc = nn.Linear(config.hidden_size, 4)
+        self.criterion = nn.CrossEntropyLoss()
+
+        self.init_weights()
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None,start_ids=None,end_ids=None):
+        outputs = self.bert(input_ids = input_ids,attention_mask=attention_mask,token_type_ids=token_type_ids)
+        sequence_output = outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        start_logits = self.start_fc(sequence_output)
+        end_logits = self.end_fc(sequence_output)
+        outputs = (start_logits,end_logits) + outputs[2:]  # add hidden states and attention if they are here
+        if start_ids is not None and end_ids is not None :
+            start_logits = start_logits.view(-1, self.num_tags)
+            end_logits = end_logits.view(-1, self.num_tags)
+
+            # 去掉 padding 部分的标签，计算真实 loss
+            active_loss = attention_mask.view(-1) == 1
+            active_start_logits = start_logits[active_loss]
+            active_end_logits = end_logits[active_loss]
+
+            active_start_labels = start_ids.view(-1)[active_loss]
+            active_end_labels = end_ids.view(-1)[active_loss]
+            start_loss = self.criterion(active_start_logits, active_start_labels)
+            end_loss = self.criterion(active_end_logits, active_end_labels)
+            loss = start_loss + end_loss
+            outputs = (loss,) + outputs
+        return outputs  # (loss), scores, (hidden_states), (attentions)
+
+

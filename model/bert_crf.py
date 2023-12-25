455e9ac74960f1c7108439b229a9521f519a4a0a
JackieWu <wujindou1@163.com>
2023-12-25 09:11:13 +0800

    Update bert_crf.py

--- model/bert_crf.py
+++ model/bert_crf.py
@@ -60,6 +60,26 @@ class BertSoftmaxForNer(BertPreTrainedModel):
         return outputs  # (loss), scores, (hidden_states), (attentions)
 
 
+class FocalLoss(nn.Module):
+    """Multi-class Focal loss implementation"""
+    def __init__(self, gamma=2, weight=None, reduction='mean', ignore_index=-100):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        self.weight = weight
+        self.ignore_index = ignore_index
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        """
+        input: [N, C]
+        target: [N, ]
+        """
+        log_pt = torch.log_softmax(input, dim=1)
+        pt = torch.exp(log_pt)
+        log_pt = (1 - pt) ** self.gamma * log_pt
+        loss = torch.nn.functional.nll_loss(log_pt, target, self.weight, reduction=self.reduction, ignore_index=self.ignore_index)
+        return loss
+
 class BertSoftmaxForSpan(BertPreTrainedModel):
     def __init__(self, config):
         super(BertSoftmaxForSpan, self).__init__(config)
@@ -68,7 +88,9 @@ class BertSoftmaxForSpan(BertPreTrainedModel):
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.start_fc = nn.Linear(config.hidden_size, 4)
         self.end_fc = nn.Linear(config.hidden_size, 4)
-        self.criterion = nn.CrossEntropyLoss(reduction='none')
+        reduction = 'none'
+        #self.criterion = nn.CrossEntropyLoss(reduction='none')
+        self.criterion = FocalLoss(reduction=reduction)
 
         self.init_weights()
 
